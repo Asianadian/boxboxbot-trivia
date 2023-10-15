a0502a257d36f4478cdbc91797e9f9763928dc05
Asianadian <danielchen.mc@gmail.com>
2023-10-15 14:13:31 -0400

    Detecting Nightbot

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,14 +18,24 @@ def login(driver):
   driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[2]/form/div/div[3]/div/button').click()
 
 def get_chat(driver):
-  last_msg = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/aside/div/div/div[2]/div/div/section/div/div[4]/div[2]/div[2]/div[3]/div/div/*[last()]")
-  print(last_msg.find_element(By.CLASS_NAME, 'chat-author__display-name').text)
+  last_msg = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "//div[@role='log']/*[last()]")))
+  try:
+    user = last_msg.find_element(By.CLASS_NAME, 'chat-author__display-name').text
+  except:
+    print('uh')
+  msg = last_msg.find_element(By.CLASS_NAME, 'text-fragment').text
+  return user, msg
 
 def is_question(user, text):
   return None
 
 def request_answer(text):
   return None
+
+def send_msg(driver, text):
+  #tbf
+  WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.TAG_NAME, "textarea"))).click().send_keys(text)
+  WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/aside/div/div/div[2]/div/div/section/div/div[6]/div[2]/div[2]/div[2]/div[3]/div/button"))).click()
 
 def main():
   options = webdriver.ChromeOptions()
@@ -34,11 +45,13 @@ def main():
   driver.get('https://www.twitch.tv/emilyywang')
 
   #Twitch hates bots
-  WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "//html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/aside/div/div/div[2]/div/div/section/div/div[4]/div[2]/div[2]/div[3]/div/div/*[last()]")))
-
+  
   while True:
-    get_chat(driver)
-    time.sleep(0.2)
+    user, msg = get_chat(driver)
+    print(user, msg)
+    if (user == 'Nightbot'):
+      send_msg(driver, "what up")
+    time.sleep(0.1)
 
   
 
