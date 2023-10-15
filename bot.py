32473f47d0f73132148230f5168e4bb3b71ddeff
Asianadian <danielchen.mc@gmail.com>
2023-10-15 15:54:53 -0400

    Only gets new messages

--- bot.py
+++ bot.py
@@ -18,11 +18,17 @@ def login(driver):
   driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[2]/form/div/div[3]/div/button').click()
 
 def get_chat(driver):
-  last_msg = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, "//div[@role='log']/*[last()]")))
+  global msg_count
   try:
-    user = last_msg.find_element(By.CLASS_NAME, 'chat-author__display-name').text
+    last_msg = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@role='log']/*[%d]" % (msg_count))))
   except:
-    print('uh')
+    print('Disconnected due to inactivity')
+    exit()
+  
+  if (last_msg.get_dom_attribute('class') != 'chat-line__message'):
+    return 'Non-message', ''
+
+  user = last_msg.find_element(By.CLASS_NAME, 'chat-author__display-name').text
   msg = last_msg.find_element(By.CLASS_NAME, 'text-fragment').text
   return user, msg
 
@@ -38,30 +44,20 @@ def send_msg(driver, text):
   WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/aside/div/div/div[2]/div/div/section/div/div[6]/div[2]/div[2]/div[2]/div[3]/div/button"))).click()
 
 def main():
+  global msg_count
+  msg_count = 1
+
   options = webdriver.ChromeOptions()
   options.add_argument("--user-data-dir=C:\\Users\\danie\\AppData\\Local\\Google\\Chrome\\User Data")
   options.add_argument('--profile-directory=Profile 6')
   driver = webdriver.Chrome(options=options)
+
   driver.get('https://www.twitch.tv/emilyywang')
 
   #Twitch hates bots
-  
   while True:
     user, msg = get_chat(driver)
     print(user, msg)
-    if (user == 'Nightbot'):
-      send_msg(driver, "what up")
-    time.sleep(0.1)
-
-  
-
-
-
-
-  
-
-
-
-
+    msg_count += 1
 
 main()
