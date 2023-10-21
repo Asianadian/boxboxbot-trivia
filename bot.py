f73c9728c9f7f57a415ee451f0cd53e251645d7c
Asianadian <danielchen.mc@gmail.com>
2023-10-21 12:14:25 -0400

    Changed scrape logic and added backend stub

--- bot.py
+++ bot.py
@@ -4,12 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+import pymongo
+
 from decouple import config
-import time
+
+import re
 
 TTV_USER = config('TTV_USER')
 TTV_PSWD = config('TTV_PSWD')
 OPENAI_KEY = config('OPENAI_KEY')
+DB_USER = config('DB_USER')
+DB_PSWD = config('DB_PSWD')
 
 def login(driver):
   driver.find_element(By.XPATH, '/html/body/div[1]/div/div[2]/nav/div/div[3]/div[3]/div/div[1]/div[1]/button').click()
@@ -18,37 +23,24 @@ def login(driver):
   driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div/div/div/div[1]/div/div/div[2]/form/div/div[3]/div/button').click()
 
 def get_chat(driver):
-  global msg_count
-
   try:
-    last_msg = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@role='log']/*[%d]" % (msg_count))))
-    first_msg = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@role='log']/*[1]")))
+    last_msg = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@role='log']/*[last()]")))
   except:
     print('Disconnected due to inactivity')
     exit()
   try:
     if (last_msg.get_dom_attribute('class') != 'chat-line__message'):
-      if msg_count < 150:
-        msg_count += 1
       return 'Non-message', ''
   except: 
-    if msg_count < 150:
-      msg_count += 1
     return 'Instantly deleted', ''
   try:
     user = last_msg.find_element(By.CLASS_NAME, 'chat-author__display-name').text
     msg = last_msg.find_element(By.CLASS_NAME, 'text-fragment').text
   except:
-    user = "blocked user"
+    user = "Blocked user"
     msg = "message deleted by a moderator"
   
-  print(user, msg)
-
-  if msg_count < 150:
-    msg_count += 1
-  else:
-    WebDriverWait(driver, 60).until(EC.staleness_of(first_msg))
-  
+  return user, msg
 
 def is_question(user, text):
   return None
@@ -62,18 +54,31 @@ def send_msg(driver, text):
   WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/aside/div/div/div[2]/div/div/section/div/div[6]/div[2]/div[2]/div[2]/div[3]/div/button"))).click()
 
 def main():
-  global msg_count
-  msg_count = 1
+  global collection
+
+  client = pymongo.MongoClient(
+  "mongodb+srv://%s:%s@bbbtrivia.82fvkwa.mongodb.net/?retryWrites=true&w=majority" % (DB_USER, DB_PSWD))
+
+  db = client["bbbquestions"]
+
+  collection = db["questions"]   
 
   options = webdriver.ChromeOptions()
   options.add_argument("--user-data-dir=C:\\Users\\danie\\AppData\\Local\\Google\\Chrome\\User Data")
   options.add_argument('--profile-directory=Profile 6')
   driver = webdriver.Chrome(options=options)
 
-  driver.get('https://www.twitch.tv/imaqtpie')
+  driver.get('https://www.twitch.tv/Thebausffs')
 
   #Twitch hates bots
   while True:
-    get_chat(driver)
+    user, msg = get_chat(driver)
+    print(user, msg)
+    if user == 'BoxBoxBot' and re.search(r'^[.]:[.]?$', msg):
+      collection.insert_one({"question":msg, "answer":""})
+    try:
+      WebDriverWait(driver, 20).until(EC.staleness_of(WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//div[@role='log']/*[1]")))))
+    except:
+      continue
 
 main()
